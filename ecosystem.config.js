d9d685720113986c9ab6200f4560e92d41b1e43e
Sandeep Dommaraju <dommar01@email.franklin.edu>
2025-06-22 13:04:01 -0400

    chore: install dev deps for build, prune production deps, add PM2 ecosystem config

---
+++ ecosystem.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  apps: [
+    {
+      name: 'portfolio',
+      cwd: '/var/www/portfolio',
+      script: 'npm',
+      args: 'start',
+      env: {
+        NODE_ENV: 'production'
+      }
+    }
+  ]
+};
